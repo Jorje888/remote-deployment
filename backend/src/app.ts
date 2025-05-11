1700564bc31fbcf2497600da06b5da82c6522c0f
Jorje <lursmanashviligiorgi88@gmail.com>
2025-05-11 23:12:35 +0400

    added post and get request responses as well as server state

---
+++ backend/src/app.ts
@@ -0,0 +1,29 @@
+import express from "express";
+const app = express();
+const port = 3001;
+
+let state: string = "Hi, I'm Echo. Say something so I can say it right back!";
+
+app.post("/api/create-answer", (req, res) => {
+  const data: string = req.body.data;
+  if (!data || data.trim() === "") {
+    res.status(400).json({ error: "No data provided" });
+    return;
+  }
+  state = data;
+  res
+    .status(200)
+    .json({ result: "Message received. The bell cannot be unrung" });
+});
+
+app.get("/api/get-answer", (req, res) => {
+  if (!state || state.trim() === "") {
+    res.status(400).json({ error: "No data provided" });
+    return;
+  }
+  res.status(200).json({ result: state });
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
