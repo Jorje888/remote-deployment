976ee8d8b8a3366590cefff121d5cdd44e015ed9
Giorgi Lursmanashvili <lursmanashvili.giorgi@tbcacademy.edu.ge>
2025-06-21 10:54:44 +0400

    added cadvisor and localized exporters to try both approaches, none work yet

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import cors from "cors";
+import { collectDefaultMetrics, register } from 'prom-client';
 const app = express();
 const port = 3001;
 app.use(cors());
+
+
+collectDefaultMetrics();
 
 let state: string = "Hi, I'm Echo. Say something so I can say it right back!";
 
@@ -24,6 +28,15 @@ app.get("/api/get-answer", (req, res) => {
     return;
   }
   res.status(200).json({ result: state });
+});
+
+app.get('/metrics', async (_req, res) => {
+  try {
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+  } catch (err) {
+    res.status(500).end(err);
+  }
 });
 
 app.listen(port, "0.0.0.0", () => {
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import viteLogo from "/vite.svg";
 import { Link } from "react-router-dom";
 import "./App.css";
 
+
 function App() {
   return (
     <>
